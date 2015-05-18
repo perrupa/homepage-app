2aa32bac0ca66fbe958d4a10e7ed5d549dc1dceb
Chris Marlow <chris.marlow@achievers.com>
2015-05-18 11:33:39 -0400

    Figuring out this marionette bullshit

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,7 +1,13 @@
-'use strict';
+// 'use strict';
 
-module.exports = {
-	start() {
-		console.log("App starting...");
+var Backbone = require('backbone');
+Backbone.$ = require('jquery');
+var Marionette = require('backbone.marionette');
+
+var App = Marionette.ItemView.extend({
+	initialize() {
+		console.log('App intializing...');
 	}
-};
+})
+
+module.exports = App;
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,4 +1,4 @@
 'use strict';
 
-var app = require("./app");
-app.start();
+var App = require("./app");
+new App();
